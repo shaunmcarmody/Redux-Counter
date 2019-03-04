ffc6ba40422b7cae8f5b3884431da20aa187109b
c87 <shaun.carmody@live.co.uk>
2019-03-04 21:23:03 +0000

    increment/decrement on click

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,6 +10,7 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
+  console.log('increment works');
   // Fill in this function
   return {
     type: INCREMENT
@@ -17,6 +18,7 @@ export const increment = () => {
 };
 
 export const decrement = () => {
+  console.log('decrement works');
   // Fill in this function
   return {
     type: DECREMENT
